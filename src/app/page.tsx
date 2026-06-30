6eabc3e45f2224eefc1a7a922d2beea8364ffee4
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-06-30 15:43:03 -0700

    Back to screen height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Example from "@/components/example";
 
 const Home = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <Example text1="Prop1" text2="Prop2" text3="Props"></Example>
     </div>
   );
