9699772799b2049db1c8484bf9e19ed5a84754de
feliciaongn <feliciaongn@gmail.com>
2026-07-05 23:36:22 -0700

    Socials Page Content

---
+++ src/app/socials/page.tsx
@@ -0,0 +1,11 @@
+import SocialsPage from "@/components/socials/SocialsPage";
+
+const Socials = () => {
+  return (
+    <div className=" ">
+      <SocialsPage> </SocialsPage>
+    </div>
+  );
+};
+
+export default Socials;
