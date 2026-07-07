4573f7d08216c6111664eba64427e9d13b48a14e
Ryan Arriola <ryanarriola00@gmail.com>
2026-07-06 21:40:25 -0700

    Add 4th prop box, restructure layout, apply cyber colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Example from "@/components/example";
 const Home = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Example text1="Prop1" text2="Prop2" text3="Props"></Example>
+      <Example
+        text1="Prop1"
+        text2="Prop2"
+        text3="Props"
+        text4="Prop2"
+      ></Example>
     </div>
   );
 };
--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,15 +1,28 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 justify-self-center bg-red-100 p-8">
-      <div className="w-1/2 bg-blue-200 p-5 text-left">{props.text1}</div>
-
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">{props.text2}</div>
+    <div className="flex w-2/3 flex-col items-center gap-4 justify-self-center rounded-md bg-black p-8">
+      <div className="bg-cyber-lightgreen w-full rounded-md p-5 text-center text-xl text-black">
+        {props.text1}
       </div>
 
-      <div className="w-1/2 bg-yellow-200 p-5">{props.text3}</div>
+      <div className="flex w-full flex-row gap-2">
+        <div className="bg-cyber-neongreen w-1/2 rounded-full p-5 text-center text-xl text-black">
+          {props.text2}
+        </div>
+        <div className="bg-cyber-darkgreen w-1/2 rounded-full p-5 text-center text-xl text-white">
+          {props.text4}
+        </div>
+      </div>
+
+      <div className="bg-cyber-darkred w-full rounded-md p-5 text-center text-xl text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
-
 export default Example;
