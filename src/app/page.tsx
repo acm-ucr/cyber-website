6dedffb3ee7409dcb4dd928a498720555ba2d319
BacoJaco <pokemine007craft@gmail,com>
2026-07-07 00:15:16 -0700

    Implement landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Landing from "@/components/home/landing";
 import Example from "@/components/example";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="flex h-screen w-full flex-col items-center justify-center">
       <Example text1="Prop1" text2="Prop2" text3="Props"></Example>
+      <Landing />
     </div>
   );
 };
