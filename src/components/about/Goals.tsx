4db36fa0361b4d37f1e9715b41bcc07ebdf2ae70
Alaya Billinger <Alaya.sanai@icloud.com>
2026-07-06 20:47:24 -0700

    fixed formatting

--- src/components/about/Goals.tsx
+++ src/components/about/Goals.tsx
@@ -1,43 +1,43 @@
 import { goalsData } from "@/data/goals";
 const Goals = () => {
-    const {
+  const {
     title,
     description,
     topics,
     topicsTitle,
     competitions,
     competitionsTitle,
-    } = goalsData;
-    return (
+  } = goalsData;
+  return (
     <div className="mx-auto max-w-3xl space-y-12 bg-black px-6 py-12 font-mono text-white">
-        <div className="space-y-4">
+      <div className="space-y-4">
         <div className="text-4xl font-bold tracking-tight">{title}</div>
         <p className="text-cyber-lightgreen max-w-2xl text-lg leading-relaxed">
-            {description}
+          {description}
         </p>
-        </div>
+      </div>
 
-        <div className="space-y-4">
+      <div className="space-y-4">
         <div className="text-3xl font-bold tracking-tight">{topicsTitle}</div>
         <div className="text-cyber-lightgreen space-y-2 pl-4 text-lg">
-            {topics.map((topic, index) => (
+          {topics.map((topic, index) => (
             <p key={index}>{topic}</p>
-            ))}
+          ))}
         </div>
-        </div>
+      </div>
 
-        <div className="space-y-4">
+      <div className="space-y-4">
         <div className="text-3xl font-bold tracking-tight">
-            {competitionsTitle}
+          {competitionsTitle}
         </div>
         <div className="text-cyber-lightgreen space-y-2 pl-4 text-lg">
-            {competitions.map((competition, index) => (
+          {competitions.map((competition, index) => (
             <p key={index}>{competition}</p>
-            ))}
+          ))}
         </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Goals;
