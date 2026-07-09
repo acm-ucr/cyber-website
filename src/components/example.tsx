3595360329bf1e440ca04d8abeac211a9b4c1b53
Ryan Arriola <ryanarriola00@gmail.com>
2026-07-08 19:09:41 -0700

    Extract props into named ExampleProps interface, remove props

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,26 +1,28 @@
-const Example = (props: {
+interface ExampleProps {
   text1: string;
   text2: string;
   text3: string;
   text4: string;
-}) => {
+}
+
+const Example = ({ text1, text2, text3, text4 }: ExampleProps) => {
   return (
     <div className="flex w-2/3 flex-col items-center gap-4 justify-self-center rounded-md bg-black p-8">
       <div className="bg-cyber-lightgreen w-full rounded-md p-5 text-center text-xl text-black">
-        {props.text1}
+        {text1}
       </div>
 
       <div className="flex w-full flex-row gap-2">
         <div className="bg-cyber-neongreen w-1/2 rounded-full p-5 text-center text-xl text-black">
-          {props.text2}
+          {text2}
         </div>
         <div className="bg-cyber-darkgreen w-1/2 rounded-full p-5 text-center text-xl text-white">
-          {props.text4}
+          {text4}
         </div>
       </div>
 
       <div className="bg-cyber-darkred w-full rounded-md p-5 text-center text-xl text-white">
-        {props.text3}
+        {text3}
       </div>
     </div>
   );
