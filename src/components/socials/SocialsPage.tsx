377f7dc40d70b0466214417e0e66f9655bea53f3
feliciaongn <feliciaongn@gmail.com>
2026-07-10 21:56:33 -0700

    Fixing newlines in SocialPages

--- src/components/socials/SocialsPage.tsx
+++ src/components/socials/SocialsPage.tsx
@@ -1,19 +1,19 @@
+import Link from "next/link";
+
 const SocialsPage = () => {
   return (
-    <div className="font-cyber-main">
-      <div className="mb-16">
+    <div className="font-cyber-main flex flex-col">
+      <div className="flex gap-3">
         <span className="text-cyber-white text-2xl font-extrabold">
           cyber@ucr:~$
         </span>
         <span className="text-cyber-lightgreen text-2xl">
-          {" "}
-          ip link | grep cyber0{" "}
+          ip link | grep cyber0
         </span>
       </div>
 
-      <div className="text-cyber-white text- mb-8 text-4xl font-bold">
-        {" "}
-        Follow our socials!{" "}
+      <div className="text-cyber-white mb-8 text-4xl font-bold">
+        Follow our socials!
       </div>
 
       <div className="font-cyber-main mb-8 text-3xl">
@@ -22,17 +22,15 @@ const SocialsPage = () => {
           <div>
             <span className="text-cyber-white"> discord: </span>
             <span className="text-cyber-lightgreen">
-              {" "}
-              Connect and chat with fellow hackers!{" "}
-              <a
+              Connect and chat with fellow hackers!<br></br>
+              <Link
                 href="https://discord.ucrcyber.org/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-[#DFFFFF] underline"
               >
-                {" "}
-                https://discord.ucrcyber.org/{" "}
-              </a>{" "}
+                https://discord.ucrcyber.org/
+              </Link>
             </span>
           </div>
         </div>
@@ -44,18 +42,16 @@ const SocialsPage = () => {
           <div>
             <span className="text-cyber-white"> linkedin: </span>
             <span className="text-cyber-lightgreen">
-              {" "}
               Join our professional network and view our history of events and
-              accomplishments.{" "}
-              <a
+              accomplishments.<br></br>
+              <Link
                 href="https://linkedin.com/company/cyberucr"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-[#DFFFFF] underline"
               >
-                {" "}
-                https://linkedin.com/company/cyberucr{" "}
-              </a>{" "}
+                https://linkedin.com/company/cyberucr
+              </Link>
             </span>
           </div>
         </div>
@@ -67,18 +63,16 @@ const SocialsPage = () => {
           <div>
             <span className="text-cyber-white"> instagram: </span>
             <span className="text-cyber-lightgreen">
-              {" "}
               Meet the board, check out some member spotlights, and ask us
-              questions.{" "}
-              <a
+              questions.<br></br>
+              <Link
                 href="https://www.instagram.com/cyber_ucr/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-[#DFFFFF] underline"
               >
-                {" "}
-                https://www.instagram.com/cyber_ucr/{" "}
-              </a>{" "}
+                https://www.instagram.com/cyber_ucr/
+              </Link>
             </span>
           </div>
         </div>
