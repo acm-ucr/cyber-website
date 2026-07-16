56b683d289fedb71fad55331cb4798562e13684a
Ryan Arriola <ryanarriola00@gmail.com>
2026-07-16 12:06:24 -0700

    Resize PartnerBadge and adjust link section sizing

--- src/components/PartnerBadge.tsx
+++ src/components/PartnerBadge.tsx
@@ -9,8 +9,8 @@ interface PartnerBadgeProps {
 
 const PartnerBadge = ({ title, description, link }: PartnerBadgeProps) => {
   return (
-    <div className="font-cyber-main bg-cyber-darkgreen border-cyber-neongreen flex items-center rounded-lg border">
-      <div className="flex flex-col gap-2 p-6">
+    <div className="font-cyber-main bg-cyber-darkgreen border-cyber-neongreen flex h-32 w-lg items-center rounded-2xl border">
+      <div className="flex flex-1 flex-col gap-2 p-6 text-center">
         <h3 className="text-cyber-white text-xl font-bold">{title}</h3>
         <p className="text-cyber-lightgreen">{description}</p>
       </div>
@@ -19,7 +19,7 @@ const PartnerBadge = ({ title, description, link }: PartnerBadgeProps) => {
         href={link}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-cyber-white hover:text-cyber-neongreen border-cyber-neongreen flex h-full items-center border-l px-6"
+        className="text-cyber-white hover:text-cyber-neongreen border-cyber-neongreen flex h-full w-14 items-center justify-center rounded-r-2xl border-l p-0.5 text-2xl transition-colors duration-300"
       >
         <ExternalLink />
       </Link>
