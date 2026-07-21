69017fbb60e6a74246fe65ede903a8c603644157
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-07-21 11:15:34 -0700

    Reapply "Merge branch 'dev' into alayab1/404Page"
    This reverts commit 8150cd0945033e32283a68d2494c2de2dbf14bd1.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,14 +6,14 @@ import { useState, useEffect, Fragment } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/cyberlogo_text.webp";
-import { useNavbarTheme } from "@/app/context/navbar-context";
+import { useTheme } from "@/app/context/theme-context";
 import MenuLogo from "@/public/Menu_Logo.svg";
 
 const Navbar = () => {
   const [time, setTime] = useState("");
   const [mounted, setMounted] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const { theme } = useNavbarTheme();
+  const { theme } = useTheme();
   const boxClasses =
     theme === "red"
       ? "bg-red-500/10 border border-red-500/30 text-red-400"
