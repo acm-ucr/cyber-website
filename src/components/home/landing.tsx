8150cd0945033e32283a68d2494c2de2dbf14bd1
charlesluo06 <charlesluo06@gmail.com>
2026-07-20 21:33:20 -0700

    Revert "Merge branch 'dev' into alayab1/404Page"
    This reverts commit 18b5f568e6e22d5b412d4c8e85d34f16d96f3f4a, reversing
    changes made to 818f04f64df26dcb68ac1179936958a45141331e.

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -5,6 +5,13 @@ import Logo from "@/public/Cyber_Logo.svg";
 const Landing = () => {
   return (
     <div className="font-cyber-main bg-cyber-black m-10 mx-auto w-4/5 overflow-hidden rounded-lg pb-9 backdrop-blur-2xl">
+      <p className="text-cyber-white p-4 text-2xl font-extrabold">
+        cyber@ucr:~$
+        <span className="font-cyber-lightgreen ml-3 font-normal">
+          cyberfetch
+        </span>
+      </p>
+
       <div className="flex flex-row gap-2 px-4">
         <Image
           src={Logo}
