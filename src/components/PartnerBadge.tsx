feb50a5e3bdd69864b63150716afb58cfb4ee2bf
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-07-21 10:28:09 -0700

    further matching the figma

--- src/components/PartnerBadge.tsx
+++ src/components/PartnerBadge.tsx
@@ -9,8 +9,8 @@ interface PartnerBadgeProps {
 
 const PartnerBadge = ({ title, description, link }: PartnerBadgeProps) => {
   return (
-    <div className="font-cyber-main flex h-32 w-lg items-center gap-4">
-      <div className="bg-cyber-darkgreen border-cyber-lightgreen flex flex-1 flex-col gap-2 rounded-2xl border p-6 text-center">
+    <div className="font-cyber-main flex h-32 w-lg items-center gap-1">
+      <div className="bg-cyber-darkgreen border-cyber-lightgreen flex flex-1 flex-col gap-2 rounded-l-2xl border p-6 text-center">
         <h3 className="text-cyber-white text-xl font-bold">{title}</h3>
         <p className="text-cyber-lightgreen">{description}</p>
       </div>
@@ -19,7 +19,7 @@ const PartnerBadge = ({ title, description, link }: PartnerBadgeProps) => {
         href={link}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-cyber-white hover:text-cyber-neongreen bg-cyber-darkgreen border-cyber-lightgreen flex h-full w-14 items-center justify-center rounded-2xl border p-0.5 text-2xl transition-colors duration-300"
+        className="text-cyber-white hover:text-cyber-neongreen bg-cyber-darkgreen border-cyber-lightgreen flex h-full w-14 items-center justify-center rounded-r-2xl border p-0.5 text-2xl transition-colors duration-300"
       >
         <ExternalLink />
       </Link>
