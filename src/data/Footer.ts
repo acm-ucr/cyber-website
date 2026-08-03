404f3118a279105ee2000827c09cbd1187cdda8a
Ryan Arriola <ryanarriola00@gmail.com>
2026-08-03 15:37:09 -0700

    Refactor footer to use mapped FooterLinks data

---
+++ src/data/Footer.ts
@@ -0,0 +1,19 @@
+import { StaticImageData } from "next/image";
+import Discord from "@/public/Discord.svg";
+import Instagram from "@/public/Instagram.svg";
+import LinkedIn from "@/public/Linkedin.svg";
+import Email from "@/public/Mail.svg";
+
+export interface FooterLink {
+  href: string;
+  icon: StaticImageData;
+  alt: string;
+  external: boolean;
+}
+
+export const footerLinks: FooterLink[] = [
+  { href: "mailto:cyberucr@gmail.com", icon: Email, alt: "Email icon", external: false },
+  { href: "https://www.instagram.com/cyber_ucr/", icon: Instagram, alt: "Instagram icon", external: true },
+  { href: "https://discord.ucrcyber.org/", icon: Discord, alt: "Discord icon", external: true },
+  { href: "https://linkedin.com/company/cyberucr", icon: LinkedIn, alt: "LinkedIn icon", external: true },
+];
