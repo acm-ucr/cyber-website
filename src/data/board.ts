e0883773105834739c297f7ebd6eeaf6922d307a
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-08-11 18:00:44 -0700

    updating Board page

--- src/data/board.ts
+++ src/data/board.ts
@@ -23,7 +23,6 @@ export const boardData: Record<string, BoardData> = {
     image: Kiana,
     link: "/board/kiana",
     bio: "Hi! I'm the President of Cyber@UCR and a Computer Science with Business Applications student. I enjoy organizing events and helping grow the club, while creating opportunities for members to get involved. I'm especially interested in Windows systems and cybersecurity, and I'm always looking to expand my knowledge and bring more hands-on projects into the club. Feel free to reach out if you have any questions or want to get involved!",
-
   },
 
   siddharth: {
